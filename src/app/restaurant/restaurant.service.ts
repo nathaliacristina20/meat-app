30d20dfbf49d523732e45f942ab1922f5bee8f5a
Nathalia <nathaliacristina20@hotmail.com>
2021-01-23 19:23:26 -0300

    create get restaurant by id and review in the restaurant service

--- src/app/restaurant/restaurant.service.ts
+++ src/app/restaurant/restaurant.service.ts
@@ -1,3 +1,4 @@
+import { Review } from './../restaurant-detail/reviews/review.model';
 import { ErrorHandler } from './../error-handler';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
@@ -12,6 +13,18 @@ export class RestaurantsService {
   getAll(): Observable<Restaurant[]> {
     return this.http
       .get<Restaurant[]>(`http://localhost:3000/restaurants`)
+      .pipe(catchError(ErrorHandler.handleError));
+  }
+
+  getById(id: string): Observable<Restaurant> {
+    return this.http
+      .get<Restaurant>(`http://localhost:3000/restaurants/${id}`)
+      .pipe(catchError(ErrorHandler.handleError));
+  }
+
+  reviewsOfRestaurant(id: string): Observable<Review>  {
+    return this.http
+      .get<Review>(`http://localhost:3000/restaurants/${id}/reviews`)
       .pipe(catchError(ErrorHandler.handleError));
   }
 }
