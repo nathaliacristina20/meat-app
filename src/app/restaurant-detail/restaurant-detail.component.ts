6f455af77b7747a4d47e9790bfce9633a1e6b323
Nathalia <nathaliacristina20@hotmail.com>
2021-01-23 19:24:30 -0300

    create restaurant detail component

---
+++ src/app/restaurant-detail/restaurant-detail.component.ts
@@ -0,0 +1,23 @@
+import { RestaurantsService } from './../restaurant/restaurant.service';
+import { Component, OnInit } from '@angular/core';
+import { Restaurant } from '../restaurant/restaurant.model';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-restaurant-detail',
+  templateUrl: './restaurant-detail.component.html'
+})
+export class RestaurantDetailComponent implements OnInit {
+  restaurant!: Restaurant;
+
+  constructor(
+    private restaurantsService: RestaurantsService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.restaurantsService
+      .getById(this.route.snapshot.params['id'])
+      .subscribe((restaurant) => (this.restaurant = restaurant));
+  }
+}
