5cebe7a80585d6c4ac4138010e8e9909347aeaf8
Nathalia <nathaliacristina20@hotmail.com>
2021-01-31 23:15:16 -0300

    add module with providers shared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -39,8 +39,8 @@ registerLocaleData(localePt);
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    CoreModule,
-    SharedModule
+    //CoreModule,
+    SharedModule.forRoot()
   ],
   providers: [
     {
--- src/app/shared.module.ts
+++ src/app/shared.module.ts
@@ -1,6 +1,9 @@
+import { OrderService } from './modules/order/order.service';
+import { RestaurantsService } from './restaurant/restaurant.service';
+import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart.service';
 import { RatingComponent } from './shared/rating/rating.component';
 import { InputComponent } from './shared/input/input.component';
-import { NgModule } from '@angular/core';
+import { ModuleWithProviders, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RadioComponent } from './shared/radio/radio.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -17,4 +20,11 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     ReactiveFormsModule,
   ],
 })
-export class SharedModule {}
+export class SharedModule {
+  static forRoot(): ModuleWithProviders<SharedModule> {
+    return {
+      ngModule: SharedModule,
+      providers: [ShoppingCartService, RestaurantsService, OrderService],
+    };
+  }
+}
